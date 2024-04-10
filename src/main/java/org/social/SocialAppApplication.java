28aeabafbc5b7e7200d7f9820adfb3d0ebfb8280
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-04-10 12:41:33 +0400

    Feature : Exclude SecurityAutoConfiguration.class

--- src/main/java/org/social/SocialAppApplication.java
+++ src/main/java/org/social/SocialAppApplication.java
@@ -2,8 +2,9 @@ package org.social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SocialAppApplication {
 
 	public static void main(String[] args) {
