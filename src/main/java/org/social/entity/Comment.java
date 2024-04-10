26ded71eee8ca9b928122a5014c0071e27755617
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-04-10 19:59:52 +0400

    Feature : Added comment entity

---
+++ src/main/java/org/social/entity/Comment.java
@@ -0,0 +1,25 @@
+package org.social.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments", schema = "app")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    private LocalDateTime createdAt;
+}
