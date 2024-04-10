21d98bea6051a9c28175eb89f8868a6da2562feb
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-04-10 19:57:54 +0400

    Feature : Added user entity

---
+++ src/main/java/org/social/entity/User.java
@@ -0,0 +1,33 @@
+package org.social.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users", schema = "app")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String password;
+
+    private LocalDate birthDate;
+}
