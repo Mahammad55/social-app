ff2a6287343d6651fc1861d5eb0fe364fdc62348
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-04-10 19:58:37 +0400

    Feature : Added post entity

---
+++ src/main/java/org/social/entity/Post.java
@@ -0,0 +1,25 @@
+package org.social.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts", schema = "app")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    private LocalDateTime createdAt;
+}
