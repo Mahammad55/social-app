6277e4616cb7d7e1cedb41bdc2fc8f64a5f8a663
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-04-11 15:47:50 +0400

    Feature : Added PostRepository

---
+++ src/main/java/org/social/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package org.social.repository;
+
+import org.social.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
