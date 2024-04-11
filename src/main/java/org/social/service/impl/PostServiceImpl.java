171259833fb6bae565147e6d5c1fa7d4b3390ab3
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-04-11 15:51:26 +0400

    Feature : Added PostService and impl class

---
+++ src/main/java/org/social/service/impl/PostServiceImpl.java
@@ -0,0 +1,10 @@
+package org.social.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.social.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+}
