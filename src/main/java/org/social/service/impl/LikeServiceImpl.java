c9cac9465046d84625435195dfe267f52a17f945
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-04-11 15:52:07 +0400

    Feature : Added LikeService and impl class

---
+++ src/main/java/org/social/service/impl/LikeServiceImpl.java
@@ -0,0 +1,10 @@
+package org.social.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.social.service.LikeService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LikeServiceImpl implements LikeService {
+}
